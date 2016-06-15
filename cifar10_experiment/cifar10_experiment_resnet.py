97c903012daf88e219b2899e4efdefb899561526
mtjuney <mtjuney@gmail.com>
2016-06-15 12:44:48 +0900

    fix

---
+++ cifar10_experiment/cifar10_experiment_resnet.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import network
+
+
+# In[14]:
+
+from keras.utils.visualize_util import model_to_dot, plot
+from IPython.display import SVG
+from keras.utils import np_utils
+from keras.callbacks import EarlyStopping
+from matplotlib import pyplot as plt
+
+plt.style.use("ggplot")
+import random
+import pickle
+
+resnet = network.resnet()
+
+from keras.datasets import cifar10
+
+batch_size = 128
+nb_epoch = 50
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+Y_train = np_utils.to_categorical(y_train, 10)
+Y_test = np_utils.to_categorical(y_test, 10)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+
+random.seed(1405)
+resnet.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+reshis = resnet.fit(X_train, Y_train,
+              batch_size=batch_size,
+              nb_epoch=nb_epoch,
+              validation_data=(X_test, Y_test))
+
+
+with open('history_resnet.dump', 'wb') as f:
+    pickle.dump(reshis.history, f, -1)
